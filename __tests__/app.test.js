5e7fab09aeca324df445c12a95f4452d35d1644b
Greg <greg19850@gmail.com>
2023-02-20 17:32:51 +0100

    Corrections made based on review comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,15 @@
 const request = require('supertest');
 const app = require('../app');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data/');
 const connection = require('../db/connection');
 
+beforeEach(() => {
+  return seed(testData);
+});
+
 afterAll(() => {
-  return connection.end();
+  connection.end();
 });
 
 describe('app', () => {
@@ -16,10 +22,21 @@ describe('app', () => {
           const { topics } = body;
 
           expect(Array.isArray(topics)).toBe(true);
+          expect(topics.length).toBe(3);
           topics.forEach(topic => {
             expect(topic.hasOwnProperty('slug')).toBe(true);
             expect(topic.hasOwnProperty('description')).toBe(true);
           });
+        });
+    });
+  });
+  describe('/api/badPath', () => {
+    it('404: GET returns not found if incorrect path given', () => {
+      return request(app)
+        .get('/api/badPath')
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe('Path not found');
         });
     });
   });
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { getTopics } = require('./controllers/newsControllers');
-const { handle500Status } = require('./controllers/errorHandlingControllers');
+const { handle500Status, handleCustomErrors } = require('./controllers/errorHandlingControllers');
 
 
 const app = express();
-app.use(express.json());
 
 app.get('/api/topics', getTopics);
+app.use('*', (req, res, next) => {
+  res.status(404).send({ msg: 'Path not found' });
+});
 
+// app.use(handleCustomErrors);
 app.use(handle500Status);
 
 module.exports = app;
