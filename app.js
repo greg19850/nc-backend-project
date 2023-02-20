2f466b836ec1ee12c754f4444a38af92ed7eb6b8
Greg <greg19850@gmail.com>
2023-02-20 15:49:02 +0100

    Task 3-GET/api/topics complete

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { getTopics } = require('./controllers/newsControllers');
+const { handle500Status } = require('./controllers/errorHandlingControllers');
 
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
+
+app.use(handle500Status);
 
 module.exports = app;
--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,8 +1,9 @@
 const { fetchTopics } = require('../models/newsModels');
 
-exports.getTopics = (req, res) => {
-
+exports.getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
     res.status(200).send({ topics });
+  }).catch((err) => {
+    next(err);
   });
 };
